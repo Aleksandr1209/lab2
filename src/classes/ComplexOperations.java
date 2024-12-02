8481ffb845871e31d0411c8e598127978dd55476
Aleksandr1209 <sanekdj123@mail.ru>
2024-12-02 18:37:20 +0300

    change fibonacci

--- src/classes/ComplexOperations.java
+++ src/classes/ComplexOperations.java
@@ -2,8 +2,15 @@ package classes;
 
 public class ComplexOperations {
     public static int fibonacci(int n) {
-        if (n <= 1) return n;
-        return fibonacci(n - 1) + fibonacci(n - 2);
+        if (n == 1) return 1;
+        int prev1 = 0, prev2 = 1;
+        int current = 0;
+        for (int i = 2; i <= n; i++) {
+            current = prev1 + prev2;
+            prev1 = prev2;
+            prev2 = current;
+        }
+        return current;
     }
 
     public static int factorial(int n) {
